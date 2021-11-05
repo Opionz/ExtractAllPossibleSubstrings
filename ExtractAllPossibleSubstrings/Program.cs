9dce290f4900a8c32296f0f3424a2962bced71ef
Opionz <ope.ajayi@hotmail.com>
2021-11-05 06:10:24 +0100

    Add project files.

---
+++ ExtractAllPossibleSubstrings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExtractAllPossibleSubstrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert text below!");
+            string input = Console.ReadLine();
+            string output = GetSubstrings(input);
+            Console.WriteLine(output);
+        }
+
+        public static string GetSubstrings(string InputText)
+        {
+            string substring = "";
+            InputText = InputText.Trim();
+            for (int i = 0; i < InputText.Length; i++)
+            {
+                for (int j = 0; j <= InputText.Length -i; j++)
+                {
+                   substring = InputText.Substring(j,i);
+                }
+            }
+            return substring;
+        }
+    }
+}
